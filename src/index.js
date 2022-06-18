18c3b38a74ad0e9f03555769c9d064f649dd547c
Doreen <wanjirudoreen13@gmail.com>
2022-06-18 11:26:42 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    fetchDogImg()
+    fetchDogBreed()
+})
+
+function fetchDogImg(){
+    fetch ("https://dog.ceo/api/breeds/image/random/4")
+.then(resp => resp.json())
+.then(data => {
+    //console.log(data)
+let images = data.message
+// console.log(images)
+images.forEach(image => {
+    //console.log(image)
+    loadImagesToDom(image)
+});
+})
+}
+
+
+function loadImagesToDom(image){
+let dogsCont = document.getElementById("dog-image-container")
+let ourImg = document.createElement('img')
+ourImg.src = image
+dogsCont.appendChild(ourImg)
+}
+
+function fetchDogBreed(){
+    fetch ('https://dog.ceo/api/breeds/list/all')
+    
+    .then (resp => resp.json())
+    .then (data => {
+        //console.log(data)
+        let breed = data.message
+        breed.forEach(breeds =>{
+            loadBreedsToDom(breeds)
+        });
+    })
+    
+}
